29295943b9d8cf86a8f30dd9c2d0d1aa9de3321e
taro.kirameki <taro.kirameki@techacademy.jp>
2019-12-20 01:39:02 +0000

    before login design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  get 'toppages/about'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  resources :toppages, only: [:index, :about]
 end
